63929360052e253c3ed99b7446703ef9f6139c11
SirWiggums <johndoe@example.com>
2024-03-21 22:26:07 +0700

    21/3 PM

--- main.js
+++ main.js
@@ -8,4 +8,4 @@ let slideIndex = 0;
 setInterval(() => {
     slideIndex = (slideIndex + 1) % 4; // Số 4 tương ứng với số lượng slides
     document.getElementById(`radio${slideIndex + 1}`).checked = true;
-}, 8000); // Thời gian chuyển đổi giữa các slide là 5 giây
+}, 8000); // Thời gian chuyển đổi giữa các slide là 8 giây
