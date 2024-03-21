e1adf752a52c2283bb8f659454d907a2ab7d7bb9
SirWiggums <johndoe@example.com>
2024-03-21 19:15:37 +0700

    21/3

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+document.querySelectorAll('.navigation-manual .manual-btn').forEach((button, index) => {
+    button.addEventListener('click', () => {
+        document.getElementById(`radio${index + 1}`).checked = true;
+    });
+});
 
-document.getElementById('next').onclick = function(){
-    let lists = document.querySelectorAll('.VP');
-    document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function(){
-    let lists = document.querySelectorAll('.VP');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+let slideIndex = 0;
+setInterval(() => {
+    slideIndex = (slideIndex + 1) % 4; // Số 4 tương ứng với số lượng slides
+    document.getElementById(`radio${slideIndex + 1}`).checked = true;
+}, 8000); // Thời gian chuyển đổi giữa các slide là 5 giây
